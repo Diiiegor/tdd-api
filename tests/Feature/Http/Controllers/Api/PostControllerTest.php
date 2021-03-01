a005b213879f66d299601355b530240ba659184d
Juan Diego <joliveros@ieo.com.co>
2021-02-28 22:28:49 -0500

    store test and sqlite database

---
+++ tests/Feature/Http/Controllers/Api/PostControllerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Api;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PostControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_store()
+    {
+        $response = $this->json('POST', '/api/posts', [
+            'title' => 'test post',
+
+        ]);
+
+        $response->assertJsonStructure(['id', 'title', 'created_at', 'updated_at'])
+            ->assertJson(['title' => 'test post'])
+            ->assertStatus(201);
+
+        $this->assertDatabaseHas('posts', ['title'=>'test post']);
+    }
+}
